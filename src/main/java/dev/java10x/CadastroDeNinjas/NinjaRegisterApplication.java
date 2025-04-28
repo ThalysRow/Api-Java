23b4380f99bfb248cc567480490837536a1ce5eb
Thalys Row <thalysrogue@gmail.com>
2025-04-27 23:03:47 -0300

    rename file

--- src/main/java/dev/java10x/CadastroDeNinjas/NinjaRegisterApplication.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaRegisterApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CadastroDeNinjasApplication {
+public class NinjaRegisterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CadastroDeNinjasApplication.class, args);
+		SpringApplication.run(NinjaRegisterApplication.class, args);
 	}
 
 }
