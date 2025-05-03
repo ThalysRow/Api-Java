2fcc61f5a0b2d63a933feb91b1f5713199d1e313
Thalys Row <thalysrogue@gmail.com>
2025-05-03 16:04:14 -0300

    feat: created missions routes

--- src/main/java/dev/java10x/CadastroDeNinjas/Missions/MissionController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missions/MissionController.java
@@ -1,4 +1,33 @@
 package dev.java10x.CadastroDeNinjas.Missions;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("mission")
 public class MissionController {
+
+    @PostMapping("/add")
+    public String addMission(){
+        return "New Mission";
+    }
+
+    @GetMapping("/ID")
+    public String showMission(){
+        return "Mission";
+    }
+
+    @GetMapping("/all")
+    public String ShowMissions(){
+        return "Missions";
+    }
+
+    @PutMapping("/ID")
+    public String updateMission(){
+        return "Update Mission";
+    }
+
+    @DeleteMapping("/ID")
+    public String deleteMission(){
+        return "Delete Mission";
+    }
 }
