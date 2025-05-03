5a30e8c97e0abe9cf6012ee03569bcbe7ffa23f5
Thalys Row <thalysrogue@gmail.com>
2025-05-03 00:17:27 -0300

    fix: add uuid generate

--- src/main/java/dev/java10x/CadastroDeNinjas/Missions/MissionModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missions/MissionModel.java
@@ -17,7 +17,7 @@ import java.util.UUID;
 public class MissionModel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
     private char rank;
