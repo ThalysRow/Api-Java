959839da3afd5f7a0596ccf6cfcb60fde717171c
Thalys Row <thalysrogue@gmail.com>
2025-05-03 16:05:29 -0300

    feat: created ninjas routes

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,15 +1,33 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("ninja")
 public class NinjaController {
 
-    @GetMapping("/boasvindas")
-    public String boasViindas(){
-        return "Bem-vindo ao cadastro Ninja";
+    @PostMapping("/add")
+    public String addNinja(){
+        return "Created Ninja";
+    }
+
+    @GetMapping("/ID")
+    public String showNinja(){
+        return "Ninja";
+    }
+
+    @GetMapping("/all")
+    public String showNinjas(){
+        return "Ninjas";
+    }
+
+    @PutMapping("/ID")
+    public String NinjaUpdate(){
+        return "Ninja Update";
+    }
+
+    @DeleteMapping("/ID")
+    public String NinjaDelete(){
+        return "Ninja Delete";
     }
 }
