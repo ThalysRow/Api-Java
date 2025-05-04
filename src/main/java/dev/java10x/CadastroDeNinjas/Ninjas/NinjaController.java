93ac4a5410b7d519836bc40d6d39f121ce818416
Thalys Row <thalysrogue@gmail.com>
2025-05-03 22:40:15 -0300

    feat: add routes find ninja and list ninjas

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,24 +1,40 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/ninja")
 public class NinjaController {
+
+    private NinjaServices ninjaServices;
+
+    public NinjaController(NinjaServices ninjaServices) {
+        this.ninjaServices = ninjaServices;
+    }
 
     @PostMapping("/add")
     public String addNinja(){
         return "Created Ninja";
     }
 
-    @GetMapping("/ID")
-    public String showNinja(){
-        return "Ninja";
+    @GetMapping("/{id}")
+    public ResponseEntity<?> showNinja(@PathVariable UUID id){
+       try {
+           NinjaModel ninja = ninjaServices.findByID(id);
+           return ResponseEntity.ok(ninja);
+       }catch (RuntimeException e){
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+       }
     }
 
     @GetMapping("/all")
-    public String showNinjas(){
-        return "Ninjas";
+    public List<NinjaModel> showNinjas(){
+        return ninjaServices.NinjaList();
     }
 
     @PutMapping("/ID")
