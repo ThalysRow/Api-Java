d9fbe0ff0c427c9fa272e645af3de833408e027a
Thalys Row <thalysrogue@gmail.com>
2025-05-03 22:38:16 -0300

    feat: add find ninja by Id

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaServices.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaServices.java
@@ -1,4 +1,24 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class NinjaServices {
+
+    private NinjaRepository ninjaRepository;
+
+    public NinjaServices(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    public List<NinjaModel> NinjaList(){
+        return ninjaRepository.findAll();
+    }
+
+    public NinjaModel findByID(UUID id){
+        return ninjaRepository.findById(id).orElseThrow(() -> new RuntimeException("Ninja not found"));
+    }
 }
