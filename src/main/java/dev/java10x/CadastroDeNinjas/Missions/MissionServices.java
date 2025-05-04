534b4c07b76a9d26b26272c19dcf5209d9d20d4c
Thalys Row <thalysrogue@gmail.com>
2025-05-03 22:43:19 -0300

    feat: add list missions

--- src/main/java/dev/java10x/CadastroDeNinjas/Missions/MissionServices.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missions/MissionServices.java
@@ -1,4 +1,18 @@
 package dev.java10x.CadastroDeNinjas.Missions;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MissionServices {
+    private MissionRepository missionRepository;
+
+    public MissionServices(MissionRepository missionRepository) {
+        this.missionRepository = missionRepository;
+    }
+
+    public List<MissionModel> missionList(){
+        return missionRepository.findAll();
+    }
 }
